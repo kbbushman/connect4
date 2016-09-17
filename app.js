1aa95f96110909cbf476162b9294d2099f9e237c
kbbushman <k.bushman@yahoo.com>
2016-09-17 00:30:03 -0700

    adds gameboard and click listener

---
+++ app.js
@@ -0,0 +1,54 @@
+
+// wait for the document to load before doing anything!
+window.onload = function() {
+
+	console.log('Document loaded... ready!');
+
+
+	//Array for bulding a 7x6 grid of cells
+	var cells = [
+		["","","","","","",""],
+		["","","","","","",""],
+		["","","","","","",""],
+		["","","","","","",""],
+		["","","","","","",""],
+		["","","","","","",""]
+	];
+
+	//Use cells array to create a grid of empty cells, then run listen function
+	function buildGameBoard() {
+		for(var i = 0; i < cells.length; i++){
+	    	for(var j = 0; j < cells[i].length; j++){
+	    		$("#gameboard").append('<td class="cell"></td>');
+	    	}
+		}
+		return listen();
+	};
+
+	buildGameBoard();
+
+
+		function listen() {
+			$('#gameboard').children().click(function() {
+				console.log(this);
+				changeColor();
+			});
+		}
+
+	// //Add a listener to the Gameboard div. Console log the child. Pass child to changeColor function.
+	// function listen(){
+	// 	$('#gameboard').on('click', this, function() {
+	// 		console.log(this); //trying to get this to return the child div of #gameboard
+	// 		changeColor(this);//then run the changeColor function.
+	// 	});
+		
+	// }
+
+	//Add inline background style to change color. Console log my failure...
+	function changeColor(element) {
+		$(this).attr('style', 'background: black');
+		console.log("I'm trying!")
+	}
+
+	
+}; //end document onload
