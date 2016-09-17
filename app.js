e63059d2ccb7ea45993e76bc82011d37f7ca5b21
kbbushman <k.bushman@yahoo.com>
2016-09-17 14:22:23 -0700

    adds buildGameBoard method to Connect4Game

--- app.js
+++ app.js
@@ -5,24 +5,14 @@ window.onload = function() {
 	console.log('Game loaded... ready!');
 
 	//Set global variables for now. Need to create constructor for player and cells, or for buildGameBoard
-	// var currentPlayer = 0;
+	 var currentPlayer = 0;
 	// console.log(currentPlayer);
-
-	//Array for bulding a 7x6 grid of cells
-	// var gameBoardCells = [
-	// 	["","","","","","",""],
-	// 	["","","","","","",""],
-	// 	["","","","","","",""],
-	// 	["","","","","","",""],
-	// 	["","","","","","",""],
-	// 	["","","","","","",""]
-	// ];
 
 	//NewGame Object
 	function Connect4Game(options) {
-		this.player = options.player,
-		this.currentPlayer = options.currentPlayer,
-		this.winner = otions.winner,
+		this.player = options.player;
+		this.currentPlayer = options.currentPlayer;
+		this.winner = options.winner;
 		gameBoardCells = [
 							["","","","","","",""],
 							["","","","","","",""],
@@ -33,11 +23,9 @@ window.onload = function() {
 						];
 	};
 
-	var options = {player: [0,1], currentPlayer: 0, winner: null};
-	var newGame = new Connect4Game(options);
 
-	//Use cells array to create a grid of empty cells, then run listen function
-	function buildGameBoard() {
+	//Connect4Game method builds dynamic gameboard cells
+	Connect4Game.prototype.buildGameBoard = function() {
 		for(var i = 0; i < gameBoardCells.length; i++){
 	    	for(var j = 0; j < gameBoardCells[i].length; j++){
 	    		$("#gameboard").append('<td class="cell" point="0"></td>');
@@ -46,7 +34,10 @@ window.onload = function() {
 		return listenForClick();
 	};
 
-	buildGameBoard();
+
+	var options = {player: [0,1], currentPlayer: 0, winner: null};
+	var newGame = new Connect4Game(options);
+	newGame.buildGameBoard();
 
 
 	function listenForClick() {
@@ -78,6 +69,21 @@ window.onload = function() {
 		currentPlayer = 0;
 		console.log(currentPlayer);
 	};
+
+
+	// //Use cells array to create a grid of empty cells, then run listen function
+	// function buildGameBoard() {
+	// 	for(var i = 0; i < gameBoardCells.length; i++){
+	//     	for(var j = 0; j < gameBoardCells[i].length; j++){
+	//     		$("#gameboard").append('<td class="cell" point="0"></td>');
+	//     	}
+	// 	}
+	// 	return listenForClick();
+	// };
+
+	// buildGameBoard();
+
+
 
 
 
