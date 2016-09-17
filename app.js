e5afc8e3e60ffd9c2f3c23bcade9f6bb6b5a987e
kbbushman <k.bushman@yahoo.com>
2016-09-17 13:52:23 -0700

    starts object oriented building

--- app.js
+++ app.js
@@ -5,23 +5,41 @@ window.onload = function() {
 	console.log('Game loaded... ready!');
 
 	//Set global variables for now. Need to create constructor for player and cells, or for buildGameBoard
-	var currentPlayer = 0;
-	console.log(currentPlayer);
+	// var currentPlayer = 0;
+	// console.log(currentPlayer);
 
 	//Array for bulding a 7x6 grid of cells
-	var cells = [
-		["","","","","","",""],
-		["","","","","","",""],
-		["","","","","","",""],
-		["","","","","","",""],
-		["","","","","","",""],
-		["","","","","","",""]
-	];
+	// var gameBoardCells = [
+	// 	["","","","","","",""],
+	// 	["","","","","","",""],
+	// 	["","","","","","",""],
+	// 	["","","","","","",""],
+	// 	["","","","","","",""],
+	// 	["","","","","","",""]
+	// ];
+
+	//NewGame Object
+	function Connect4Game(options) {
+		this.player = options.player,
+		this.currentPlayer = options.currentPlayer,
+		this.winner = otions.winner,
+		gameBoardCells = [
+							["","","","","","",""],
+							["","","","","","",""],
+							["","","","","","",""],
+							["","","","","","",""],
+							["","","","","","",""],
+							["","","","","","",""]
+						];
+	};
+
+	var options = {player: [0,1], currentPlayer: 0, winner: null};
+	var newGame = new Connect4Game(options);
 
 	//Use cells array to create a grid of empty cells, then run listen function
 	function buildGameBoard() {
-		for(var i = 0; i < cells.length; i++){
-	    	for(var j = 0; j < cells[i].length; j++){
+		for(var i = 0; i < gameBoardCells.length; i++){
+	    	for(var j = 0; j < gameBoardCells[i].length; j++){
 	    		$("#gameboard").append('<td class="cell" point="0"></td>');
 	    	}
 		}
@@ -48,9 +66,8 @@ window.onload = function() {
 		var clickedCell = element;
 		$(clickedCell).attr('point', '1').html("1");
 		$(clickedCell).attr('style', 'background: red');
-		currentPlayer = 0;
+		currentPlayer = 1;
 		console.log(currentPlayer);
-		console.log(cells);
 	};
 
 	//Add point attribute to cell. Change background to red. Increment and console log currentPlayer.
@@ -58,9 +75,10 @@ window.onload = function() {
 		var clickedCell = element;
 		$(clickedCell).attr('point', '2').html("2");
 		$(clickedCell).attr('style', 'background: black');
-		currentPlayer = 1;
+		currentPlayer = 0;
 		console.log(currentPlayer);
 	};
+
 
 
 	
