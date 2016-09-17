b8c4700706449c0573fdebc028d8e6502982a498
kbbushman <k.bushman@yahoo.com>
2016-09-17 02:47:03 -0700

    adds alternate player function

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 // wait for the document to load before doing anything!
 window.onload = function() {
 
-	console.log('Document loaded... ready!');
+	console.log('Game loaded... ready!');
 
+	//Set global variables
+	var currentPlayer = 0;
 
 	//Array for bulding a 7x6 grid of cells
 	var cells = [
@@ -19,37 +21,46 @@ window.onload = function() {
 	function buildGameBoard() {
 		for(var i = 0; i < cells.length; i++){
 	    	for(var j = 0; j < cells[i].length; j++){
-	    		$("#gameboard").append('<td class="cell"></td>');
+	    		$("#gameboard").append('<td class="cell" point="0"></td>');
 	    	}
 		}
-		return listen();
+		return listenForClick();
 	};
 
 	buildGameBoard();
 
 
-		function listen() {
-			$('#gameboard').children().click(this, function() {
-				console.log(this);
-				changeColor(this);
-			});
-		}
+	function listenForClick() {
+		$('#gameboard').children().click(this, function() {
+			if (currentPlayer % 2 == 0){
+				changeGridRed(this);
+			} else {
+				changeGridBlack(this);
+			}
+		})
+	};
 
-	// //Add a listener to the Gameboard div. Console log the child. Pass child to changeColor function.
-	// function listen(){
-	// 	$('#gameboard').on('click', this, function() {
-	// 		console.log(this); //trying to get this to return the child div of #gameboard
-	// 		changeColor(this);//then run the changeColor function.
-	// 	});
-		
-	// }
-
-	//Add inline background style to change color. Console log my failure...
-	function changeColor(element) {
+	//Add point attribute to cell. Change background to red. Increment and console log currentPlayer.
+	//Maybe a run the for loop to update the cells array, or create a new array for checking win status.
+	//Maybe an if statement to clean up currentPlayer update and remove repetative code.
+	function changeGridRed(element) {
 		var clickedCell = element;
+		$(clickedCell).attr('point', '1').html("1");
+		$(clickedCell).attr('style', 'background: red');
+		currentPlayer = currentPlayer + 1;
+		console.log(currentPlayer);
+		console.log(cells);
+	};
+
+	//Add point attribute to cell. Change background to red. Increment and console log currentPlayer.
+	function changeGridBlack(element) {
+		var clickedCell = element;
+		$(clickedCell).attr('point', '2');
 		$(clickedCell).attr('style', 'background: black');
-		console.log(this)
-	}
+		currentPlayer = currentPlayer + 1;
+		console.log(currentPlayer);
+	};
+
 
 	
 }; //end document onload
